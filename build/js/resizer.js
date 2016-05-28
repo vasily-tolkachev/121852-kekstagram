c7cfad5addce84b894c613fc7aa0197457122f45
vasily-tolkachev <vasilytolkachew@gmail.com>
2016-05-28 21:27:59 +0300

    Добавил темный фон и вывод размеров

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -107,6 +107,7 @@
 
       var displX = -(this._resizeConstraint.x + this._resizeConstraint.side / 2);
       var displY = -(this._resizeConstraint.y + this._resizeConstraint.side / 2);
+
       // Отрисовка изображения на холсте. Параметры задают изображение, которое
       // нужно отрисовать и координаты его верхнего левого угла.
       // Координаты задаются от центра холста.
@@ -119,6 +120,32 @@
           (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
+
+      var clippingRegion = new Square(
+          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth,
+          (this._resizeConstraint.side + this._ctx.lineWidth / 2));
+
+      this._ctx.beginPath();
+      this._ctx.moveTo(-this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(-this._container.width / 2, this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(-this._container.width / 2, -this._container.height / 2);
+
+      this._ctx.moveTo(clippingRegion.x, clippingRegion.y);
+      this._ctx.lineTo(clippingRegion.x + clippingRegion.side, clippingRegion.y);
+      this._ctx.lineTo(clippingRegion.x + clippingRegion.side, clippingRegion.y + clippingRegion.side);
+      this._ctx.lineTo(clippingRegion.x, clippingRegion.y + clippingRegion.side);
+      this._ctx.lineTo(clippingRegion.x, clippingRegion.y);
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.fill('evenodd');
+
+      this._ctx.fillStyle = 'white';
+      this._ctx.font = '16px Tahoma';
+      this._ctx.textAlign = 'center';
+      this._ctx.textBaseline = 'bottom';
+      this._ctx.fillText(this._image.naturalWidth + ' x ' + this._image.naturalHeight, 0, clippingRegion.y - 5);
 
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
